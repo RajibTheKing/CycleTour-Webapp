afffe2d22dc681bcb264f0dce080db8ec06b09cc
nayemmajhar <nayemmajhar@gmail.com>
2020-01-12 19:57:12 +0100

    add react-leaflet maker and routing

---
+++ apps/ctweb/src/pages/Help/components/Routing.js
@@ -0,0 +1,66 @@
+import { MapLayer } from "react-leaflet";
+import L from "leaflet";
+import "leaflet-routing-machine";
+import "lrm-graphhopper"
+import { withLeaflet } from "react-leaflet";
+
+class Routing extends MapLayer {
+  createLeafletElement() {
+    const { map } = this.props;
+  
+    let leafletElement = new L.Routing.Control({
+			waypoints: [
+			L.latLng( 54.322818512961135, 10.143492221832275),
+			L.latLng( 54.338740125896415, 10.12313961982727)
+			],
+			
+			router: new L.Routing.GraphHopper( '42ba18ee-a255-4fc3-b945-2cec9410907f' , {
+				urlParameters: {
+				vehicle: 'bike'
+				}
+      }),
+      lineOptions: {
+        styles: [
+          {
+            color: "blue",
+            opacity: 0.6,
+            weight: 4
+          }
+        ]
+      },
+      addWaypoints: false,
+      draggableWaypoints: false,
+      fitSelectedRoutes: false,
+      showAlternatives: false
+    }).addTo(map.leafletElement);
+
+    new L.Routing.Control({
+			waypoints: [
+			L.latLng( 54.322818512961135, 10.143492221832275),
+			L.latLng( 54.338740125896415, 10.12313961982727)
+			],
+			
+			router: new L.Routing.GraphHopper( '42ba18ee-a255-4fc3-b945-2cec9410907f' , {
+				urlParameters: {
+				vehicle: 'foot'
+				}
+      }),
+      lineOptions: {
+        styles: [
+          {
+            color: "red",
+            opacity: 0.6,
+            weight: 4
+          }
+        ]
+      },
+      addWaypoints: false,
+      draggableWaypoints: false,
+      fitSelectedRoutes: false,
+      showAlternatives: false
+    }).addTo(map.leafletElement);
+
+    return leafletElement.getPlan();
+  }
+}
+export default withLeaflet(Routing);
--- apps/ctweb/src/pages/Help/index.js
+++ apps/ctweb/src/pages/Help/index.js
@@ -1,35 +1,50 @@
-import React, { Component } from 'react'
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import React, { Component } from "react";
+import L from 'leaflet'
+import { Map, TileLayer, withLeaflet, MapControl, Popup, Marker } from "react-leaflet";
 
-type State = {
-    lat: number,
-    lng: number,
-    zoom: number,
+import Routing from "./components/Routing";
+import areaData from './components/areaData'
+
+class Help extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      lat: 54.32313137415068,
+      lng: 10.139522552490234,
+      zoom: 13,
+      isMapInit: false
+    };
+  }
+
+  saveMap = map => {
+    this.map = map;
+    this.setState({
+      isMapInit: true
+    });
+  };
+
+  render() {
+    const { lat, lng, zoom } = this.state;
+    const position = [lat, lng];
+
+    return (
+      <Map center={position} zoom={zoom} ref={this.saveMap}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        {this.state.isMapInit && <Routing map={this.map} />}
+        <Marker position={[54.322818512961135, 10.143492221832275]}>
+          <Popup> Kiel Maritime Museum </Popup>
+        </Marker>
+
+        <Marker position={[54.338740125896415, 10.12313961982727]}>
+          <Popup> Kiel University </Popup>
+        </Marker>
+
+      </Map>
+    );
+  }
 }
 
-export default class Help extends Component{
-    state = {
-    lat: 51.505,
-    lng: -0.09,
-    zoom: 13,
-    }
-
-    render() {
-    const position = [this.state.lat, this.state.lng]
-        return (
-            <div className="">
-                <Map center={position} zoom={this.state.zoom}>
-                <TileLayer
-                    attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker position={position}>
-                    <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
-                </Map>
-            </div>
-        )
-    }
-}
+export default Help;
